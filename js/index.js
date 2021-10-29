3e47fae137b853f030b9bb4d17a7f33f677d6161
SilverioSimoes <silverio.simoes@gmail.com>
2021-10-29 16:01:24 -0300

    Music Player(Em desenvolvimento)

--- js/index.js
+++ js/index.js
@@ -1,57 +1,85 @@
 let i = 0
+let playing = false
 const image = document.querySelector('#image')
-const audio = document.querySelector('#audio')
+const audio = new Audio(data[i].file)
+const currentAudio = audio
+const audioTime = document.querySelector('#audio-time')
+const audioDuration = document.querySelector('#audio-duration')
 const title = document.querySelector('#title')
 const artist = document.querySelector('#artist')
 const previous = document.querySelector('#previous')
+const playElement = document.querySelector('#play')
+const pauseElement = document.querySelector('#pause')
+const stopElement = document.querySelector('#stop')
 const next = document.querySelector('#next')
-const Stop = document.querySelector('#stop')
-const currentAudio = data[i].file
 
 function currentAudioData() {
   image.src = data[i].image
   audio.src = data[i].file
+  currentAudio
   title.innerText = data[i].title
   artist.innerText = data[i].artist
+  audioTime.innerText = currentAudio.currentTime
+  audioDuration.innerText = currentAudio.duration
 }
 
 function playAudio() {
-  audio.play()
+  playing = true
+  currentAudio.play()
+}
+
+function pauseAudio() {
+  playing = false
+  currentAudio.pause()
 }
 
 function prevAudio() {
   i--
   if (i >= 0) {
-    currentAudioData()
+    if (playing) {
+      currentAudioData()
+      playAudio()
+    } else currentAudioData()
   } else {
     i = data.length - 1
-    currentAudioData()
+    if (playing) {
+      currentAudioData()
+      playAudio()
+    } else currentAudioData()
   }
 }
 
 function nextAudio() {
   i++
   if (i < data.length) {
-    currentAudioData()
+    if (playing) {
+      currentAudioData()
+      playAudio()
+    } else {
+      currentAudioData()
+    }
   } else {
     i = 0
-    currentAudioData()
+    if (playing) {
+      currentAudioData()
+      playAudio()
+    } else currentAudioData()
   }
-}
-function startAudio() {
-  playAudio()
 }
 
 function stopAudio() {
+  playing = false
   currentAudioData()
 }
 
 currentAudioData()
 
-audio.addEventListener('ended', () => {
+currentAudio.addEventListener('ended', () => {
   nextAudio()
   playAudio()
 })
 previous.addEventListener('click', () => prevAudio())
+playElement.addEventListener('click', () => playAudio())
+pauseElement.addEventListener('click', () => pauseAudio())
+stopElement.addEventListener('click', () => stopAudio())
 next.addEventListener('click', () => nextAudio())
-Stop.addEventListener('click', () => stopAudio())
